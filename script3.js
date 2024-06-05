c09ff4b7a82aec3d6a16ffcd7417f7573ec97c9c
CoderKnight077 <166790123+CoderKnight077@users.noreply.github.com>
2024-06-05 12:21:02 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('toggler');
+
+const navLinks = document.getElementById('nav-Links');
+
+toggler.addEventListener('click'  , ()  =>   {
+
+
+   navLinks.classList.toggle('active');
+} )
